f58f26d9b9c46a5d0f4facf1b2a03d192e3aee0e
Wolf Benjamin <Benjamin.Wolf@swisscom.com>
2018-10-26 22:35:29 +0200

    Migrate player view to mongodb aswell

--- player/views.py
+++ player/views.py
@@ -1,9 +1,9 @@
-from rest_framework import viewsets
+from rest_framework_mongoengine import viewsets
 from .serializers import PlayerSerializer
 from .models import Player
 
 
-class PlayerViewSet(viewsets.ModelViewSet):
+class PlayerViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that allows player to be viewed
     """
