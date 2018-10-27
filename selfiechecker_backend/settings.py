d6c0ec2253f7be88cb7901db3daf28cb123c2145
Wolf Benjamin <Benjamin.Wolf@swisscom.com>
2018-10-27 08:20:57 +0200

    Try to fix static files on azure

--- selfiechecker_backend/settings.py
+++ selfiechecker_backend/settings.py
@@ -17,7 +17,7 @@ import mongoengine
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-AZURE_DIR = '/home/site/wwwroot'
+AZURE_DIR = 'D:\home\site\wwwroot'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -157,6 +157,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(AZURE_DIR, "static")
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(AZURE_DIR, 'static')
 ]
