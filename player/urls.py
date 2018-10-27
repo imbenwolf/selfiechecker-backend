25f83662412523d90a35e4934569d3bce46a9873
Wolf Benjamin <Benjamin.Wolf@swisscom.com>
2018-10-27 04:58:00 +0200

    Fix small issue with cors

--- player/urls.py
+++ player/urls.py
@@ -3,5 +3,5 @@ from .views import PlayerView, UpdatePlayerView
 
 urlpatterns = [
     path('', PlayerView.as_view()),
-    path('update/', UpdatePlayerView.as_view()),
+    path('update', UpdatePlayerView.as_view()),
 ]
--- selfiechecker_backend/settings.py
+++ selfiechecker_backend/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -55,6 +56,8 @@ MIDDLEWARE = [
 ]
 
 APPEND_SLASH = False
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'selfiechecker_backend.urls'
 
